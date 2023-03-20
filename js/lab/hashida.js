4424ba85bdf2cef7f265a77776b05d5e9422a0b4
AraiYuki226 <cy19208@shibaura-it.ac.jp>
2023-03-20 11:47:52 +0900

    0320 詳細追加

---
+++ js/lab/hashida.js
@@ -0,0 +1,156 @@
+$("#open-1").modaal({
+    content_source: '#hashida-1',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-2").modaal({
+    content_source: '#hashida-2',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-3").modaal({
+    content_source: '#hashida-3',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-4").modaal({
+    content_source: '#hashida-4',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-5").modaal({
+    content_source: '#hashida-5',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-6").modaal({
+    content_source: '#hashida-6',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-7").modaal({
+    content_source: '#hashida-7',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-8").modaal({
+    content_source: '#hashida-8',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-9").modaal({
+    content_source: '#hashida-9',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-10").modaal({
+    content_source: '#hashida-10',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-11").modaal({
+    content_source: '#hashida-11',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-12").modaal({
+    content_source: '#hashida-12',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
+$("#open-13").modaal({
+    content_source: '#hashida-13',
+    overlay_close:true,
+	animation_speed:150,
+    before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+})
+
